851fb1d1b65b8f8eed41707c7f95690e5c87bcb4
Tim Vlasuk <timvlasuk@gmail.com>
2019-06-14 10:50:42 -0700

    using direction operators to eliminate or direct errors

--- backup1.sh
+++ backup1.sh
@@ -4,7 +4,7 @@ user=$(whoami)
 input=/home/$user/Documents
 output=/tmp/${user}_docs_$(date +%Y-%m-%d+%H%M%S).tar.gz
 
-tar -czf $output $input
+tar -czf $output $input 2> /dev/null
 
 echo "Backup of $input completed! Details about the output file:"
 ls -l $output
