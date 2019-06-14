67f809bac39993985090ec47baa201f208c634de
Tim Vlasuk <timvlasuk@gmail.com>
2019-06-14 11:14:47 -0700

    added to backup script with functions

--- backup1.sh
+++ backup1.sh
@@ -4,8 +4,24 @@ user=$(whoami)
 input=/home/$user/Documents
 output=/tmp/${user}_docs_$(date +%Y-%m-%d+%H%M%S).tar.gz
 
+function total_files {
+	find $1 -type f | wc -l
+}
+
+function total_directories {
+	find $1 -type d | wc -l
+}
+
 tar -czf $output $input 2> /dev/null
 
-echo "Backup of $input completed! Details about the output file:"
+echo -n "Number of files to be included:"
+total_files $input
+
+echo -n "Number of directories to be included:"
+total_directories $input
+
+
+echo "Backup of $input completed!"
+echo " Details about the output file:"
 ls -l $output
 
