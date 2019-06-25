f9547d7d289a3a4bff8840a47d2fc9ccdd7b7bcd
Tim Vlasuk <timvlasuk@gmail.com>
2019-06-24 17:10:01 -0700

    modified forLoop

--- forLoop.sh
+++ forLoop.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-for i in 1 2 3; do
-	echo $i
+if [ -z $1 ]; then
+	echo "Requires a file to read character count of each line!"
+fi
+
+for i in $( cat $1 ); do
+	echo -n $i | wc -c
 done
---
+++ test.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/perl
+
+use warnings;
+
+$name = "TimBoT";
+my $name2 = "BoY";
+
+print "hi $name\n $name2\n";
+
