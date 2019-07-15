12e22eef90c55ddb4fc79a9744ccc8107a124899
Tim Vlasuk <timvlasuk@gmail.com>
2019-07-14 23:26:36 -0700

    hands on with read

---
+++ readComm.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Let's see how many variables we can catch per word"
+read l1 l2 l3
+
+# the third variable collects any/all words after the first and second
+echo "first was $l1"
+echo "second was $l2"
+echo "third was $l3"
+
